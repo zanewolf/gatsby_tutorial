586c2b9f632ee6e6f403755667fe6be9c67ebc41
zanewolf <zwolf.mlxvi@gmail.com>
2021-08-11 14:36:33 -0400

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>
+        About Page
+      </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacinia non tellus id sagittis. Maecenas quis tempor justo. Mauris et ante augue. Integer interdum turpis vel nulla efficitur, vel euismod nisl condimentum. Morbi suscipit, augue in posuere congue, quam turpis semper ligula, quis condimentum lorem ante ut tellus. Praesent sed lacus justo. Suspendisse dignissim blandit felis, et feugiat elit dignissim vel. Proin sed ultrices ex. Integer vestibulum malesuada diam a egestas. </p>
+    </div>
+
+  )
+}
+
+
